7602c10869cb6709550e1027f2212e8d630ac71f
Rishabh-CMD23 <rishabhsolanki1707@gmail.com>
2024-04-25 14:02:04 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -1,7 +1,26 @@
 function longestSubstring(s) {
     // Implementation of longestSubstring function
+    const charSet = new Set();
+    
+    let left = 0;
+    let right = 0;
+
+    let maxLength = 0;
+
+    while (right < s.length) {
+        if (!charSet.has(s[right])) {
+            charSet.add(s[right]);
+            maxLength = Math.max(maxLength, right - left + 1);
+            right++;
+        } else {
+            charSet.delete(s[left]);
+            left++;
+        }
+    }
+    return maxLength;
+
+
 }
 
 module.exports = { longestSubstring };
-
 
--- program2.js
+++ program2.js
@@ -1,7 +1,23 @@
 function smallestMissingPositiveInteger(nums) {
 
-  // Implement the function smallest_missing_positive_integer
+  if (nums.length === 0) return 1;
+  if (nums.length === 1) return nums[0] <= 0 ? 1 : 2;
 
+  let n = nums.length;
+  for (let i = 0; i < n; i++) {
+    
+    while (0 < nums[i] <= n && nums[i] != nums[nums[i] - 1]) {
+      let j = nums[i] - 1;
+      [nums[i], nums[j]] = [nums[j], nums[i]];
+    }
+  }
+
+  for (let i = 0; i < n; i++) {
+    if (nums[i] != i + 1) {
+      return i + 1;
+    }
+  }
+  return n + 1;
 }
 
 module.exports = smallestMissingPositiveInteger;
